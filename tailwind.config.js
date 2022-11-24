bfbe90c31eff85bdf0c09f1c04aa2201811ea5e1
WaiLinnAung18 <waia00198@gmail.com>
2022-11-24 13:37:15 +0630

    add hover effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
         secondary: "#0369a1",
         seclight: "#4cc9f0",
         secdark: "#075985",
+        facebook: "#3b5998",
+        igtop: "#8a3ab9",
+        igmid: "#bc2a8d",
+        igb: "#fbad50",
+        twitter: "#00acee",
       },
       fontFamily: {
         primaryFf: ["Rokkitt"],
